e58967c24a2ef64c4db3725e814cd46de5721e2d
MFALLAM <mohammed.fathi.allam@gmail.com>
2022-10-21 17:26:16 +0300

    Setting title of NumberActivity

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,4 +15,25 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
     }
+
+    public void openNumberList(View view) {
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFamilyMembersList(View view) {
+        Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+        startActivity(intent);
+    }
+
+    public void openColorsList(View view) {
+        Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPhrasesList(View view) {
+        Intent intent = new Intent(MainActivity.this, PhraseActivity.class);
+        startActivity(intent);
+    }
+
 }
