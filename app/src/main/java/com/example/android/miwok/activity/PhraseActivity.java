8bb40995eec6c1d7f74f0c2295977727ae9ddef8
MFALLAM <mohammed.fathi.allam@gmail.com>
2022-10-27 11:15:43 +0300

    Removed padding right & padding left from activities (colors, family, numbers, phrases)

--- app/src/main/java/com/example/android/miwok/activity/PhraseActivity.java
+++ app/src/main/java/com/example/android/miwok/activity/PhraseActivity.java
@@ -21,6 +21,17 @@ public class PhraseActivity extends AppCompatActivity {
 
         ArrayList<Phrase> phrases = new ArrayList<>();
 
+        phrases.add(new Phrase("minto wuksus", "Where are you going?"));
+        phrases.add(new Phrase("tinnә oyaase'nә", "What is your name?"));
+        phrases.add(new Phrase("oyaaset", "My name is.."));
+        phrases.add(new Phrase("michәksәs?", "How are you feeling?"));
+        phrases.add(new Phrase("kuchi achit?", "I’m feeling good."));
+        phrases.add(new Phrase("Are you coming?", "әәnәs'aa?"));
+        phrases.add(new Phrase("Yes, I’m coming.", "hәә’ әәnәm."));
+        phrases.add(new Phrase("I’m coming.", "әәnәm."));
+        phrases.add(new Phrase("Let’s go.", "yoowutis."));
+        phrases.add(new Phrase("Come here.", "әnni'nem."));
+
         // Create a link WordAdapter, whose data source is a list of objects
         PhraseAdapter phraseAdapter = new PhraseAdapter(PhraseActivity.this, 0, phrases);
 
--- app/src/main/java/com/example/android/miwok/adapter/PhraseAdapter.java
+++ app/src/main/java/com/example/android/miwok/adapter/PhraseAdapter.java
@@ -1,13 +1,16 @@
 package com.example.android.miwok.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.android.miwok.R;
 import com.example.android.miwok.data.Phrase;
 
 import java.util.List;
@@ -20,6 +23,34 @@ public class PhraseAdapter extends ArrayAdapter<Phrase> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        // Checking if the existing view is begin reused, otherwise inflate the view.
+        // if null inflate the layout
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        /**
+         * Get the {@link Colors} object located at this position in the list
+         */
+        Phrase currentObject = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_textview
+        TextView miwokTextView = listItemView.findViewById(R.id.miwok_textview);
+
+        // set this text on the name TextView
+        // Get the name from the current Color object and
+        miwokTextView.setText(currentObject.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_textview
+        TextView defaultTextView = listItemView.findViewById(R.id.default_textview);
+
+        // set this text on the name TextView
+        // Get the name from the current Color object and
+        defaultTextView.setText(currentObject.getDefaultTranslation());
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
     }
 }
